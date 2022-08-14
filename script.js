f2f657fe6e9c471be00e44f2bc8e85d363cb19ac
naraharidhivi <111236432+naraharidhivi@users.noreply.github.com>
2022-08-14 12:52:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkButton() {
+    if (document.getElementById("option1").checked) {
+      document.getElementById("reveal-if-active").innerHTML = "";
+    } 
+    else if (document.getElementById("option2").checked) {
+      document.getElementById("reveal-if-active").innerHTML = "";
+    } 
+    else if (document.getElementById("option3").checked) {
+      document.getElementById("reveal-if-active").innerHTML =
+        "<br> <label for='hourlyRate'>Hourly Rate</label><input type='text' id='hourlyRate' name='hourlyRate' placeholder='Hourly Rate here...' required pattern='[0-9]+'/>";
+    }
+  }
+  
